5dc3d1690e6be3c1619e51e9affbad7ce4c0f5a6
추민수 <devsoftychoo@gmail.com>
2024-02-14 21:22:05 +0900

    [Feat] : 데이터클래스 Note

---
+++ app/src/main/java/com/example/note_mvvm_compose/feature/domain/model/Note.kt
@@ -0,0 +1,23 @@
+package com.example.note_mvvm_compose.feature.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.note_mvvm_compose.theme.BabyBlue
+import com.example.note_mvvm_compose.theme.LightGreen
+import com.example.note_mvvm_compose.theme.RedOrange
+import com.example.note_mvvm_compose.theme.RedPink
+import com.example.note_mvvm_compose.theme.Violet
+
+// note 정보를 다룰 data Class
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
