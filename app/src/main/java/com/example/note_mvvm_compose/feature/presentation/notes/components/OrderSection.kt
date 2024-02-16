153fe73599108952c94dda230a5facc39037758c
추민수 <devsoftychoo@gmail.com>
2024-02-16 17:03:07 +0900

    [Feat] : 상단 정렬 UI OrderSection
    RadioButton모음
    title, date, color, Descending, Ascending으로 정렬

---
+++ app/src/main/java/com/example/note_mvvm_compose/feature/presentation/notes/components/OrderSection.kt
@@ -0,0 +1,75 @@
+package com.example.note_mvvm_compose.feature.presentation.notes.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.RadioButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.note_mvvm_compose.Strings
+import com.example.note_mvvm_compose.feature.domain.util.NoteOrder
+import com.example.note_mvvm_compose.feature.domain.util.OrderType
+
+@Composable
+fun OrderSection(
+    modifier: Modifier = Modifier,
+    noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+    onOrderChange: (NoteOrder) -> Unit
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            DefaultRadioButton(
+                text = Strings.BTN_TEXT_TITLE,
+                checked = noteOrder is NoteOrder.Title,
+                onSelect = { onOrderChange(NoteOrder.Title(noteOrder.orderType)) })
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = Strings.BTN_TEXT_DATE,
+                checked = noteOrder is NoteOrder.Date,
+                onSelect = { onOrderChange(NoteOrder.Date(noteOrder.orderType)) })
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = Strings.BTN_TEXT_COLOR,
+                checked = noteOrder is NoteOrder.Color,
+                onSelect = { onOrderChange(NoteOrder.Color(noteOrder.orderType)) })
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            DefaultRadioButton(
+                text = Strings.BTN_TEXT_ASCENDING,
+                checked = noteOrder.orderType is OrderType.Ascending,
+                onSelect = {
+                    onOrderChange(noteOrder.copy(OrderType.Ascending))
+                }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = Strings.BTN_TEXT_DESCENDING,
+                checked = noteOrder.orderType is OrderType.Descending,
+                onSelect = {
+                    onOrderChange(noteOrder.copy(OrderType.Descending))
+                }
+            )
+        }
+
+    }
+
+}
+
+
+@Preview(name = "PreviewOrderSection")
+@Composable
+fun PreviewOrderSection() {
+    OrderSection { }
+}
