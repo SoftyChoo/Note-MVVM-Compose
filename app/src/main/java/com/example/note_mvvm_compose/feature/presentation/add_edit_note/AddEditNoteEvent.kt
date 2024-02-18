aab604dcf280ed0bfb55a24fd2b7d9da39701318
추민수 <devsoftychoo@gmail.com>
2024-02-18 22:57:13 +0900

    [Feat] : AddEditNoteEvent
    추가, 수정을 할 때의 Event 추가

---
+++ app/src/main/java/com/example/note_mvvm_compose/feature/presentation/add_edit_note/AddEditNoteEvent.kt
@@ -0,0 +1,22 @@
+package com.example.note_mvvm_compose.feature.presentation.add_edit_note
+
+import androidx.compose.ui.focus.FocusState
+
+
+/*
+FocusState interface
+Inactive: 포커스를 가지고 있지 않은 비활성 상태
+Active: 포커스를 가지고 있는 활성 상태
+Focused: 포커스를 가지고 있고 사용자가 해당 요소에 입력을 할 수 있는 상태
+ */
+sealed class AddEditNoteEvent {
+    data class EnteredTitle(val value: String) : AddEditNoteEvent()
+    data class ChangeTitleFocus(val focusState: FocusState) : AddEditNoteEvent()
+
+    data class EnteredContent(val value: String) : AddEditNoteEvent()
+    data class ChangeContentFocus(val focusState: FocusState) : AddEditNoteEvent()
+
+    data class ChangeColor(val color: Int) : AddEditNoteEvent()
+    object SaveNote: AddEditNoteEvent()
+
+}
