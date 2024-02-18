492ed5e2a6278b2d7fe41a67b4e024215f562d12
추민수 <devsoftychoo@gmail.com>
2024-02-18 22:55:21 +0900

    [Feat] : NoteUseCases 에 Usecase추가

--- app/src/main/java/com/example/note_mvvm_compose/feature/domain/usecase/NoteUseCases.kt
+++ app/src/main/java/com/example/note_mvvm_compose/feature/domain/usecase/NoteUseCases.kt
@@ -3,8 +3,8 @@ package com.example.note_mvvm_compose.feature.domain.usecase
 
 // ViewModel 에 UseCase 를 주입해 줄 클래스
 class NoteUseCases(
-    val getNoteUseCase: GetNoteUseCase,
+    val getNotesUseCase: GetNotesUseCase,
     val deleteNoteUseCase: DeleteNoteUseCase,
-    val addNoteUseCase: AddNoteUseCase
-) {
-}
+    val addNoteUseCase: AddNoteUseCase,
+    val getNoteUseCase: GetNoteUseCase
+)
